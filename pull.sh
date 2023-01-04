f51d648ac1964cc60c27370661cd03ee5acc5fbc
= <=>
2023-01-04 11:56:02 +0800

    update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 spawn git fetch origin Koying-dev
 
-spawn git diff FETCH_HEAD
+spawn git diff origin/Koying-dev
 expect {
   eof {send_user "Allready up to date!!!\n";exit 0}
   "diff*"
